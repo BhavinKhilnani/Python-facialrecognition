b703bf7d2a8124f7aa1fdeeef9564b46ad6ee02a
BhavinKhilnani <142653210+BhavinKhilnani@users.noreply.github.com>
2026-01-30 20:57:03 +0530

    Delete ards.py

--- ards.py
+++
@@ -1,8 +0,0 @@
-ranks = ["Ace", "2", "3"," 4", "5"," 6", "7", "8", "9", "10", "Jack", "Queen","King"]
-suits = ["hearts", "diamonds", "clubs", "spades"]
-class Cards:
-    def __init__(self,rank,suit):
-        self.rank = rank
-        self.suit - suit
-
-
